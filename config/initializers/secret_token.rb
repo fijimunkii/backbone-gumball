bea27dd6e5a69a5c0efe2b178f02a9bfd9dc4d3e
Harrison Powers <fijimunkii@gmail.com>
2013-11-26 11:51:14 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneCandymachineJs::Application.config.secret_token = 'd049c6d2f4f5291e52eb2c2dc11066109998cabfa40b1e0d5f5bceed44cb4e3cf04cda311eb05b6fa2927c38888f2ba8b716232f6a06bbaff9c81abe56776be9'
